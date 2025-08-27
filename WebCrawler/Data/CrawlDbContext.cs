cce76b08a1e97fcc06bd32b67560917cb4ce0f20
Reza <mre.navazani@gmail.com>
2025-08-27 13:18:05 -0700

    Initial commit with correct structure

---
+++ WebCrawler/Data/CrawlDbContext.cs
@@ -0,0 +1,13 @@
+﻿// Data/CrawlDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebCrawler.Api.Data
+{
+    public class CrawlDbContext : DbContext
+    {
+        public CrawlDbContext(DbContextOptions<CrawlDbContext> options) : base(options) { }
+
+        public DbSet<PageResult> Pages { get; set; }
+    }
+}
